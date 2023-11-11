69099df679f2b08877e1488de696bd5aef5419a1
Eduardo Garcia Venegas <edugarcia.venegas@gmail.com>
2023-11-10 22:44:30 -0300

    Proyecto con ejercicios Unidad 3 corregido

--- EvaluacionUnidad3/Ejercicio2/Ejercicio2.py
+++ EvaluacionUnidad3/Ejercicio2/Ejercicio2.py
@@ -2,7 +2,6 @@ import funcion as fc
 
 #Una función que reciba un numero “N” como parámetro y que retorne un arreglo (lista) con “N” notas ingresadas por el usuario.
 n=int(input("Indique la cantidad de notas a ingresar en sistema --> "))
-#fc.completar_arreglo(n)
 print("===============================")
 #Una función que reciba como parámetro una lista (arreglo) que siempre tendrá elementos numéricos (nunca estará vacía) y
 #que retorne el promedio de los elementos de dicha lista.
